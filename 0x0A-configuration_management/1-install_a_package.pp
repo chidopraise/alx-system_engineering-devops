74b43127f4f5fdba4ad6645431b8a5cf6bb3d724
Abraham Livinus <abrahamlivinus@gmail.com>
2024-04-19 16:27:55 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install Flask version 2.1.0 using pip3
+# Install a flask package
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+package {'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
