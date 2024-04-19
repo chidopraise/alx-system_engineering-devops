0cf0c9f8b9fc97b8d0756bea17d3f80baffa7623
Abraham Livinus <abrahamlivinus@gmail.com>
2024-04-19 15:50:09 -0700

    puppet apply 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install a flask package
+# Ensure the package 'python3-pip' is installed
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Execute pip3 to install Flask version 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  # Specify that the command should only run if Flask 2.1.0 is not already installed
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],  # Ensure python3-pip is installed first
 }
