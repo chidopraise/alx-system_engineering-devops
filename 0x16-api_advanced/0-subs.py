a032d2ed08a5d1ea11a2334f530c7ed985b323d0
Abraham Livinus <abrahamlivinus@gmail.com>
2024-06-04 13:27:54 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+'''
+returns number of subscribers for a given subreddit
+'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    user_agent = {'User-agent': 'greg'}
+    sub = requests.get('http://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user_agent)
+    try:
+        sub = sub.json().get('data')
+        for k, v in sub.items():
+            if k == 'subscribers':
+                return(v)
+    except:
+        return(0)
